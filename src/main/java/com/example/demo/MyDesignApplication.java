7c4ee552101697c7b0db2867a7246201755fd139
hf <hf@192.168.58.1>
2019-10-24 13:29:22 +0800

    设计模式

---
+++ src/main/java/com/example/demo/MyDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDesignApplication.class, args);
+	}
+
+}
